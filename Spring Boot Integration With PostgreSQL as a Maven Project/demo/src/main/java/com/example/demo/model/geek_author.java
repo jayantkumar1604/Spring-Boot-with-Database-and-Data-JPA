9800c5f1af639ddf22fd77b4fb184a3a3e2b25a3
Jayant Kumar <123481470+jayantkumar1604@users.noreply.github.com>
2025-04-30 11:54:17 +0530

    Postgresql
    up

---
+++ Spring Boot Integration With PostgreSQL as a Maven Project/demo/src/main/java/com/example/demo/model/geek_author.java
@@ -0,0 +1,44 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name="geek_author")
+public class geek_author {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    private Integer author_id;
+
+    private String author_name;
+
+    private String email_id;
+
+    public Integer getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(Integer author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+}
